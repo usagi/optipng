09c21a9ec389a772944b786b747c35a35743ebce
Usagi Ito <usagi@WonderRabbitProject.net>
2015-01-15 02:06:40 +0900

    clone

---
+++ src/opnglib/include/opnglib/optk.h
@@ -0,0 +1,21 @@
+/**
+ * @file opnglib/optk.h
+ *
+ * @brief
+ * OPTK is a Portable Tool Kit.
+ *
+ * Copyright (C) 2001-2012 Cosmin Truta.
+ *
+ * This software is distributed under the zlib license.
+ * Please see the accompanying LICENSE file, or visit
+ * http://www.opensource.org/licenses/zlib-license.php
+ **/
+
+#ifndef OPNGLIB_OPTK_H
+#define OPNGLIB_OPTK_H
+
+#include "optk/bits.h"
+#include "optk/integer.h"
+#include "optk/io.h"
+
+#endif  /* OPNGLIB_OPTK_H */
